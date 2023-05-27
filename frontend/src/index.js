c5be6a9a228ab7a1579ebcc2c732c5065d5d5aa2
vinicozzi <vinicozzimincent@gmail.com>
2023-05-27 16:00:20 -0400

    redux-store setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import configureStore from './store';
+// import csrfFetch, { restoreCSRF } from './store/csrf';
+// import * as sessionActions from './store/session'
+
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store 
+  // window.csrfFetch= csrfFetch;
+  // window.sessionActions = sessionActions;
+} 
+// const renderApplication = () => {
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// }
+
+// if (sessionStorage.getItem("X-CSRF-Token") === null) {
+//   restoreCSRF().then(renderApplication);
+// } else {
+//   renderApplication();
+// }
+
+function Root() {
+
+  return ( 
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  )
+
+}
+
