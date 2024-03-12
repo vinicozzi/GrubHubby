2e232aefc6bdde20f838dfdcef73b85f1af36c37
Vincent Minicozzi <vinicozzimincent@gmail.com>
2024-03-12 12:23:57 -0400

    add manifest

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails"
-require 'sprockets/railtie'
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
@@ -13,6 +12,7 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
