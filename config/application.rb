363d99a319f8228cd263010b5f2935d66489af5b
Vincent Minicozzi <vinicozzimincent@gmail.com>
2024-03-12 12:40:27 -0400

    com

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -23,14 +22,7 @@ module GrubHubby
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.railties_order = [:all, :main_app]
 
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore,
-    key: '_GrubHubby_session',
-    same_site: :lax, 
-    secure: Rails.env.production?
-    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Jbuilder.key_format camelize: :lower
-Jbuilder.deep_format_keys true
-
-# Rails.application.routes.default_url_options = {
-#     host: 'localhost',
-#     port: 3000
-#   }
